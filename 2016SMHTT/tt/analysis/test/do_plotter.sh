5ef7f12ef2ac248f89a44af5588f975c16d9ad1d
Doyeong Kim <doyeong.kim@cern.ch>
2018-10-17 14:54:44 +0200

    fix: input in do_plotter

--- 2016SMHTT/tt/analysis/test/do_plotter.sh
+++ 2016SMHTT/tt/analysis/test/do_plotter.sh
@@ -4,32 +4,32 @@
  ####################
 
  # 1. Data
- ./tt_templateMaker.exe outputs_nominal/data.root outputs_forPlots/data.root data_obs data_obs nominal
+ ./tt_templateMaker.exe outputs_nominal/data.root outputs_forPlots/data.root data_obs nominal
 
  # 2. Signals
- ./tt_templateMaker.exe outputs_nominal/VBF125.root outputs_forPlots/VBF125.root VBF125 VBF125 nominal
- ./tt_templateMaker.exe outputs_nominal/ggH125.root outputs_forPlots/ggH125.root ggH125 ggH125 nominal
- ./tt_templateMaker.exe outputs_nominal/WH125.root outputs_forPlots/WH125.root WplusH125 WH125 nominal
- ./tt_templateMaker.exe outputs_nominal/ZH125.root outputs_forPlots/ZH125.root ZH125 ZH125 nominal
+ ./tt_templateMaker.exe outputs_nominal/VBF125.root outputs_forPlots/VBF125.root VBF125 nominal
+ ./tt_templateMaker.exe outputs_nominal/ggH125.root outputs_forPlots/ggH125.root ggH125 nominal
+ ./tt_templateMaker.exe outputs_nominal/WH125.root outputs_forPlots/WH125.root WH125 nominal
+ ./tt_templateMaker.exe outputs_nominal/ZH125.root outputs_forPlots/ZH125.root ZH125 nominal
 
  # 3. DY
- ./tt_templateMaker.exe outputs_nominal/ZL.root outputs_forPlots/ZL.root DY ZL nominal
- ./tt_templateMaker.exe outputs_nominal/ZJ.root outputs_forPlots/ZJ.root DY ZJ nominal
+ ./tt_templateMaker.exe outputs_nominal/ZL.root outputs_forPlots/ZL.root ZL nominal
+ ./tt_templateMaker.exe outputs_nominal/ZJ.root outputs_forPlots/ZJ.root ZJ nominal
 
  # 4. EWKZ
- ./tt_templateMaker.exe outputs_nominal/EWKZ.root outputs_forPlots/EWKZ.root EWKZLL EWKZ nominal
+ ./tt_templateMaker.exe outputs_nominal/EWKZ.root outputs_forPlots/EWKZ.root EWKZ nominal
 
  # 5. TT
- ./tt_templateMaker.exe outputs_nominal/TTJ.root outputs_forPlots/TTJ.root TT TTJ nominal
+ ./tt_templateMaker.exe outputs_nominal/TTJ.root outputs_forPlots/TTJ.root TTJ nominal
 
  # 6. W
- ./tt_templateMaker.exe outputs_nominal/W.root outputs_forPlots/W.root W W nominal
+ ./tt_templateMaker.exe outputs_nominal/W.root outputs_forPlots/W.root W nominal
 
  # 7. VV
- ./tt_templateMaker.exe outputs_nominal/VVJ.root outputs_forPlots/VVJ.root VV VVJ nomianl
+ ./tt_templateMaker.exe outputs_nominal/VVJ.root outputs_forPlots/VVJ.root VVJ nomianl
 
  # embedded
- ./tt_templateMaker.exe outputs_nominal/embedded.root outputs_forPlots/embedded.root embedded embedded nominal
+ ./tt_templateMaker.exe outputs_nominal/embedded.root outputs_forPlots/embedded.root embedded nominal
 
  python python/SignalCreator.py
  python python/QCDcreator.py
