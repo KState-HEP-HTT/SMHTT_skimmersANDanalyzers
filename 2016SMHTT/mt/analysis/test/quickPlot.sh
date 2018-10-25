c2444c829b21ebeb94b1d423224eb99e0fd3946c
Doyeong Kim <doyeong.kim@cern.ch>
2018-10-25 12:38:22 +0200

    add: ROC curve maker which work with quick plotter

--- 2016SMHTT/mt/analysis/test/quickPlot.sh
+++ 2016SMHTT/mt/analysis/test/quickPlot.sh
@@ -1,16 +1,16 @@
 #!/bin/bash                                                                                                                                                    
-rm plots/Quick/*
+rm plots/Quick/*.root
 
 export input=""
-for i in `ls files_nominal/*.root`; do
+for i in `ls $1/*.root`; do
     export input="$iput $i"
     #echo $input                                                                                                                                               
     fileName="${input%.*}"
     fileName="${fileName##*/}"
     #echo $fileName                                                                                                                                 
-    ./quickplot.exe $input plots/Quick/$fileName.root $fileName $1 $2 $3 $4
+    ./quickplot.exe $input plots/Quick/$fileName.root $fileName $2 $3 $4 $5
 done
 
 python python/QCDcreator.py -i plots/Quick/ -q
-hadd -f final_nominal.root plots/Quick/*
-python python/stackPlotter_dev.py
+hadd -f plots/Quick/templates/$5.root plots/Quick/*.root
+#python python/stackPlotter_dev.py
