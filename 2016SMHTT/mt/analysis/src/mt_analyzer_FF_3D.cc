6661c67243923ccf76335de8ac5dfd2a3f3b57f1
abdollah <smohamadi@gmail.com>
2018-11-16 21:17:35 -0600

    update the Fake Factor

--- 2016SMHTT/mt/analysis/src/mt_analyzer_FF_3D.cc
+++ 2016SMHTT/mt/analysis/src/mt_analyzer_FF_3D.cc
@@ -90,7 +90,9 @@ int main(int argc, char** argv) {
     // The "FakeFraction_mutau_3D.root" file will be the output of the _Get_Fake_Fraction.py
     // Thus, this part is not neccessary to derive the fake fraction.
     // This part is only needed when we want to run the code, estimate jet fake background and produce final results.
+    //FF
     ////////////////////////////////////////////////////////////////////////////////
+    bool computeFakeFraction = true;
     TFile *FakeFraction=new TFile("FakeFraction_mutau_3D.root");
     TH3F *fakefrac_0jet=(TH3F*) FakeFraction->Get("0jet/0jet");
     TH3F *fakefrac_boosted=(TH3F*) FakeFraction->Get("boosted/boosted");
@@ -438,12 +440,24 @@ int main(int argc, char** argv) {
     //float bins_taupt[] = {30,35,40,300};
     float bins_taupt[] = {0,10,20,30,40,50,60,70,80,90,100,110,120,130,140,150,160,170,180,190,200};
     
+    
+    
+    
+    float bins_mvis_FF[] ={0,50,80,100,110,120,130,150,170,200,250,1000};
+    float bins_njet_FF[] = {-0.5,0.5,1.5,15};
+    
+    
+    
     int  binnum1 = sizeof(bins1)/sizeof(Float_t) - 1;
     int  binnum2 = sizeof(bins2)/sizeof(Float_t) - 1;
     int  binnum0 = sizeof(bins0)/sizeof(Float_t) - 1;
     int  binnum_pth = sizeof(bins_pth)/sizeof(Float_t) - 1;
     int  binnum_taupt = sizeof(bins_taupt)/sizeof(Float_t) - 1;
     int  binnum_mjj = sizeof(bins_mjj)/sizeof(Float_t) - 1;
+    
+    int  binnum_mvis_FF = sizeof(bins_mvis_FF)/sizeof(Float_t) - 1;
+    int  binnum_njet_FF = sizeof(bins_njet_FF)/sizeof(Float_t) - 1;
+    
     
     TH1F *hincl=new TH1F ("hincl", "hincl", 16,0,160); hincl->Sumw2();
     TH1F *nlowhigh=new TH1F ("nlowhigh", "nlowhigh", 6,0,6);nlowhigh->Sumw2();
@@ -547,7 +561,7 @@ int main(int argc, char** argv) {
 
     //FakeFactor boosted FF
     std::vector<TH1F*> h1_AI_FF;
-    std::vector<TH1F*> h1_AI_FakeFraction_2D;
+    std::vector<TH2F*> h1_AI_FakeFraction_2D;
     
     //FakeFactor vbf FF
     std::vector<TH1F*> h2_AI_FF;
@@ -661,19 +675,20 @@ int main(int argc, char** argv) {
         //////////////////////////////////////////////////////
         // Defining the histogram for Fake fraction computation
         //////////////////////////////////////////////////////
+                
         
         
         //FakeFactor  0jet FF
         ostringstream HNSh0_AI_FakeFraction_2D; HNSh0_AI_FakeFraction_2D << "h0_AI_FakeFraction_2D" << k;
-        h0_AI_FakeFraction_2D.push_back(new TH1F (HNSh0_AI_FakeFraction_2D.str().c_str(),"FakeJet_0jet",binnum_mjj,bins_mjj)); h0_AI_FakeFraction_2D[k]->Sumw2();
+        h0_AI_FakeFraction_2D.push_back(new TH2F (HNSh0_AI_FakeFraction_2D.str().c_str(),"FakeJet_0jet",binnum_mvis_FF,bins_mvis_FF,binnum_njet_FF,bins_njet_FF)); h0_AI_FakeFraction_2D[k]->Sumw2();
         
         //FakeFactor  boosted FF
         ostringstream HNSh1_AI_FakeFraction_2D; HNSh1_AI_FakeFraction_2D << "h1_AI_FakeFraction_2D" << k;
-        h1_AI_FakeFraction_2D.push_back(new TH1F (HNSh1_AI_FakeFraction_2D.str().c_str(),"FakeJet_boosted",binnum_mjj,bins_mjj)); h1_AI_FakeFraction_2D[k]->Sumw2();
+        h1_AI_FakeFraction_2D.push_back(new TH2F (HNSh1_AI_FakeFraction_2D.str().c_str(),"FakeJet_boosted",binnum_mvis_FF,bins_mvis_FF,binnum_njet_FF,bins_njet_FF)); h1_AI_FakeFraction_2D[k]->Sumw2();
         
         //FakeFactor  vbf FF
         ostringstream HNSh2_AI_FakeFraction_2D; HNSh2_AI_FakeFraction_2D << "h2_AI_FakeFraction_2D" << k;
-        h2_AI_FakeFraction_2D.push_back(new TH1F (HNSh2_AI_FakeFraction_2D.str().c_str(),"FakeJet_vbf",binnum_mjj,bins_mjj)); h2_AI_FakeFraction_2D[k]->Sumw2();
+        h2_AI_FakeFraction_2D.push_back(new TH2F (HNSh2_AI_FakeFraction_2D.str().c_str(),"FakeJet_vbf",binnum_mvis_FF,bins_mvis_FF,binnum_njet_FF,bins_njet_FF)); h2_AI_FakeFraction_2D[k]->Sumw2();
 
 
         //////////////////////////////////////////////////////
@@ -717,19 +732,11 @@ int main(int argc, char** argv) {
     //###############################################################
 
 
-   TFile* ff_file_0jet = TFile::Open("HTTutilities/Jet2TauFakes/data/mt/0Jet/fakeFactors_20180831_tight.root");
-   FakeFactor* ff_0jet = (FakeFactor*)ff_file_0jet->Get("ff_comb");
-   const std::vector<std::string>& inputNames_0jet = ff_0jet->inputs();
-    
-   TFile* ff_file_boosted = TFile::Open("HTTutilities/Jet2TauFakes/data/mt/boosted/fakeFactors_20180831_tight.root");
-   FakeFactor* ff_boosted = (FakeFactor*)ff_file_boosted->Get("ff_comb");
-   const std::vector<std::string>& inputNames_boosted = ff_boosted->inputs();
-    
-   TFile* ff_file_vbf = TFile::Open("HTTutilities/Jet2TauFakes/data/mt/vbf/fakeFactors_20180831_tight.root");
-   FakeFactor* ff_vbf = (FakeFactor*)ff_file_vbf->Get("ff_comb");
-   const std::vector<std::string>& inputNames_vbf = ff_vbf->inputs();
+   TFile* ff_file_0jet = TFile::Open("../../../../../HTTutilities/Jet2TauFakes/data/mt/0Jet/fakeFactors_20180831_tight.root");
+   FakeFactor* fakeFactorCmb = (FakeFactor*)ff_file_0jet->Get("ff_comb");
     
    std::vector<double> inputs(9);
+    
    std::string FFsys[31]={"","ff_qcd_syst_up","ff_qcd_syst_down","ff_qcd_dm0_njet0_stat_up","ff_qcd_dm0_njet0_stat_down","ff_qcd_dm0_njet1_stat_up","ff_qcd_dm0_njet1_stat_down","ff_qcd_dm1_njet0_stat_up","ff_qcd_dm1_njet0_stat_down","ff_qcd_dm1_njet1_stat_up","ff_qcd_dm1_njet1_stat_down","ff_w_syst_up","ff_w_syst_down","ff_w_dm0_njet0_stat_up","ff_w_dm0_njet0_stat_down","ff_w_dm0_njet1_stat_up","ff_w_dm0_njet1_stat_down","ff_w_dm1_njet0_stat_up","ff_w_dm1_njet0_stat_down","ff_w_dm1_njet1_stat_up","ff_w_dm1_njet1_stat_down","ff_tt_syst_up","ff_tt_syst_down","ff_tt_dm0_njet0_stat_up","ff_tt_dm0_njet0_stat_down","ff_tt_dm0_njet1_stat_up","ff_tt_dm0_njet1_stat_down","ff_tt_dm1_njet0_stat_up","ff_tt_dm1_njet0_stat_down","ff_tt_dm1_njet1_stat_up","ff_tt_dm1_njet1_stat_down"};
   TString postfixFF[31]={"","_norm_ff_qcd_mt_systUp","_norm_ff_qcd_mt_systDown","_norm_ff_qcd_dm0_njet0_mt_statUp","_norm_ff_qcd_dm0_njet0_mt_statDown","_norm_ff_qcd_dm0_njet1_mt_statUp","_norm_ff_qcd_dm0_njet1_mt_statDown","_norm_ff_qcd_dm1_njet0_mt_statUp","_norm_ff_qcd_dm1_njet0_mt_statDown","_norm_ff_qcd_dm1_njet1_mt_statUp","_norm_ff_qcd_dm1_njet1_mt_statDown","_norm_ff_w_mt_systUp","_norm_ff_w_mt_systDown","_norm_ff_w_dm0_njet0_mt_statUp","_norm_ff_w_dm0_njet0_mt_statDown","_norm_ff_w_dm0_njet1_mt_statUp","_norm_ff_w_dm0_njet1_mt_statDown","_norm_ff_w_dm1_njet0_mt_statUp","_norm_ff_w_dm1_njet0_mt_statDown","_norm_ff_w_dm1_njet1_mt_statUp","_norm_ff_w_dm1_njet1_mt_statDown","_norm_ff_tt_mt_systUp","_norm_ff_tt_mt_systDown","_norm_ff_tt_dm0_njet0_mt_statUp","_norm_ff_tt_dm0_njet0_mt_statDown","_norm_ff_tt_dm0_njet1_mt_statUp","_norm_ff_tt_dm0_njet1_mt_statDown","_norm_ff_tt_dm1_njet0_mt_statUp","_norm_ff_tt_dm1_njet0_mt_statDown","_norm_ff_tt_dm1_njet1_mt_statUp","_norm_ff_tt_dm1_njet1_mt_statDown"};
 
@@ -836,8 +843,6 @@ int main(int argc, char** argv) {
 	sf_id_anti=myScaleFactor_idAnti->get_ScaleFactor(pt_1,eta_1);
       }
 
-        
-        cout >>"antiisoRegion = "<<antiisoRegion<<"\n";
         
       float ratioanti=(sf_id_anti)/(sf_id);
       
@@ -1240,105 +1245,116 @@ int main(int argc, char** argv) {
 //############################################################################################################
 //############################################################################################################
 
-
-        //************************* Fill histograms Fake Factor 0 jet **********************
         
-        if (numberJets==0 && mt<50 && q_1*q_2<0){
+        
+        if (computeFakeFraction){
             
-            if (antiisoRegion ){ //anti-isolated data, and MC without jets to subtract later
-                h0_AI_FakeFraction_2D[k]->Fill(x_axis_var,y_axis_var,aweight*weight2);
-            }
-        }
-        //************************* Fill histograms Fake Factor boosted **********************
-        
-        if (is_boosted && mt<50 && q_1*q_2<0){
+            //************************* Fill histograms Fake Factor 0 jet **********************
             
-            if (antiisoRegion ){ //anti-isolated data, and MC without jets to subtract later
-                h1_AI_FakeFraction_2D[k]->Fill(x_axis_var,y_axis_var,aweight*weight2);
+            if (numberJets==0 && mt<50 && q_1*q_2<0){
+                
+                if (antiisoRegion ){ //anti-isolated data, and MC without jets to subtract later
+                    h0_AI_FakeFraction_2D[k]->Fill(x_axis_var,y_axis_var,aweight*weight2);
+                }
             }
-        }
-        //************************* Fill histograms Fake Factor VBF **********************
-        
-        if (is_VBF && mt<50 && q_1*q_2<0){
+            //************************* Fill histograms Fake Factor boosted **********************
             
-            if (antiisoRegion ){ //anti-isolated data, and MC without jets to subtract later
-                h2_AI_FakeFraction_2D[k]->Fill(x_axis_var,y_axis_var,aweight*weight2);
+            if (is_boosted && mt<50 && q_1*q_2<0){
+                
+                if (antiisoRegion ){ //anti-isolated data, and MC without jets to subtract later
+                    h1_AI_FakeFraction_2D[k]->Fill(x_axis_var,y_axis_var,aweight*weight2);
+                }
             }
+            //************************* Fill histograms Fake Factor VBF **********************
+            
+            if (is_VBF && mt<50 && q_1*q_2<0){
+                
+                if (antiisoRegion ){ //anti-isolated data, and MC without jets to subtract later
+                    h2_AI_FakeFraction_2D[k]->Fill(x_axis_var,y_axis_var,aweight*weight2);
+                }
+            }
+            //***********************************************
+            
         }
         
-	  
-//############################################################################################################
-//############################################################################################################
-//  THis portion is for filling the histograms whill help us to estimate the Jet Fake background.
-//############################################################################################################
-//############################################################################################################
-//############################################################################################################
+        
+        //############################################################################################################
+        //############################################################################################################
+        //  THis portion is for filling the histograms whill help us to estimate the Jet Fake background.
+        //############################################################################################################
+        //############################################################################################################
+        //############################################################################################################
         //********************* Compute fake factors *********************
         
-        inputs[0] = mytau.Pt();
-        inputs[1] = l2_decayMode;
-        inputs[2] = numberJets;
-        inputs[3] = (mymu+mytau).M();
-        inputs[4] = mt;
-        inputs[5] = iso_1;
-        // Just dommy numbers
-        //        inputs[6] = frac_qcd;
-        //        inputs[7] = frac_w;
-        //        inputs[8] = frac_tt;
-        inputs[6] = 0.30;
-        inputs[7] = 0.50;
-        inputs[8] = 0.05;
-        
-        
-        if (is_0jet){
-            inputs[6] = fakefrac_0jet->GetBinContent(fakefrac_0jet->GetXaxis()->FindBin(x_axis_var), fakefrac_0jet->GetYaxis()->FindBin(y_axis_var),1);
-            inputs[7] = fakefrac_0jet->GetBinContent(fakefrac_0jet->GetXaxis()->FindBin(x_axis_var), fakefrac_0jet->GetYaxis()->FindBin(y_axis_var),2);
-            inputs[8] = fakefrac_0jet->GetBinContent(fakefrac_0jet->GetXaxis()->FindBin(x_axis_var), fakefrac_0jet->GetYaxis()->FindBin(y_axis_var),3);
-}
-
-        if (is_boosted){
-            inputs[6] = fakefrac_boosted->GetBinContent(fakefrac_boosted->GetXaxis()->FindBin(x_axis_var), fakefrac_boosted->GetYaxis()->FindBin(y_axis_var),1);
-            inputs[7] = fakefrac_boosted->GetBinContent(fakefrac_boosted->GetXaxis()->FindBin(x_axis_var), fakefrac_boosted->GetYaxis()->FindBin(y_axis_var),2);
-            inputs[8] = fakefrac_boosted->GetBinContent(fakefrac_boosted->GetXaxis()->FindBin(x_axis_var), fakefrac_boosted->GetYaxis()->FindBin(y_axis_var),3);
-    }
-
-        
-        if (is_VBF){
-            inputs[6] = fakefrac_vbf->GetBinContent(fakefrac_vbf->GetXaxis()->FindBin(x_axis_var), fakefrac_vbf->GetYaxis()->FindBin(y_axis_var),1);
-            inputs[7] = fakefrac_vbf->GetBinContent(fakefrac_vbf->GetXaxis()->FindBin(x_axis_var), fakefrac_vbf->GetYaxis()->FindBin(y_axis_var),2);
-            inputs[8] = fakefrac_vbf->GetBinContent(fakefrac_vbf->GetXaxis()->FindBin(x_axis_var), fakefrac_vbf->GetYaxis()->FindBin(y_axis_var),3);
-      }
-        
-
-        //*********************  fake factors weight *********************
-        double FF_weight=ff_0jet->value(inputs);
-
-        //************************* Fill histograms Fake Factor 0 jet **********************
-        
-        if (numberJets==0 && mt<50 && q_1*q_2<0){
+        if ( ! computeFakeFraction){
             
-            if (antiisoRegion ){ //anti-isolated data, and MC without jets to subtract later
+            inputs[0] = mytau.Pt();
+            inputs[1] = l2_decayMode;
+            inputs[2] = numberJets;
+            inputs[3] = (mymu+mytau).M();
+            inputs[4] = mt;
+            inputs[5] = iso_1;
+            // Just dommy numbers
+            //        inputs[6] = frac_qcd;
+            //        inputs[7] = frac_w;
+            //        inputs[8] = frac_tt;
+            inputs[6] = 0.30;
+            inputs[7] = 0.50;
+            inputs[8] = 0.05;
+            
+            
+            if (is_0jet){
+                inputs[6] = fakefrac_0jet->GetBinContent(fakefrac_0jet->GetXaxis()->FindBin(x_axis_var), fakefrac_0jet->GetYaxis()->FindBin(y_axis_var),1);
+                inputs[7] = fakefrac_0jet->GetBinContent(fakefrac_0jet->GetXaxis()->FindBin(x_axis_var), fakefrac_0jet->GetYaxis()->FindBin(y_axis_var),2);
+                inputs[8] = fakefrac_0jet->GetBinContent(fakefrac_0jet->GetXaxis()->FindBin(x_axis_var), fakefrac_0jet->GetYaxis()->FindBin(y_axis_var),3);
+            }
+            
+            if (is_boosted){
+                inputs[6] = fakefrac_boosted->GetBinContent(fakefrac_boosted->GetXaxis()->FindBin(x_axis_var), fakefrac_boosted->GetYaxis()->FindBin(y_axis_var),1);
+                inputs[7] = fakefrac_boosted->GetBinContent(fakefrac_boosted->GetXaxis()->FindBin(x_axis_var), fakefrac_boosted->GetYaxis()->FindBin(y_axis_var),2);
+                inputs[8] = fakefrac_boosted->GetBinContent(fakefrac_boosted->GetXaxis()->FindBin(x_axis_var), fakefrac_boosted->GetYaxis()->FindBin(y_axis_var),3);
+            }
+            
+            
+            if (is_VBF){
+                inputs[6] = fakefrac_vbf->GetBinContent(fakefrac_vbf->GetXaxis()->FindBin(x_axis_var), fakefrac_vbf->GetYaxis()->FindBin(y_axis_var),1);
+                inputs[7] = fakefrac_vbf->GetBinContent(fakefrac_vbf->GetXaxis()->FindBin(x_axis_var), fakefrac_vbf->GetYaxis()->FindBin(y_axis_var),2);
+                inputs[8] = fakefrac_vbf->GetBinContent(fakefrac_vbf->GetXaxis()->FindBin(x_axis_var), fakefrac_vbf->GetYaxis()->FindBin(y_axis_var),3);
+            }
+            
+            
+            //*********************  fake factors weight *********************
+            double FF_weight=fakeFactorCmb->value(inputs);
+            
+            //************************* Fill histograms Fake Factor 0 jet **********************
+            
+            if (numberJets==0 && mt<50 && q_1*q_2<0){
+                
+                if (antiisoRegion ){ //anti-isolated data, and MC without jets to subtract later
+                    //                for (int k=l;k<l+nbhistFF;++k){ // To get also the systematics
                     h0_AI_FF[k]->Fill(FinalObservable,aweight*weight2*FF_weight);
+                    //            }
+                }
             }
-        }
-        //************************* Fill histograms Fake Factor boosted **********************
-        
-        if (is_boosted && mt<50 && q_1*q_2<0){
+            //************************* Fill histograms Fake Factor boosted **********************
             
-            if (antiisoRegion ){ //anti-isolated data, and MC without jets to subtract later
-                //                for (int k=l;k<l+nbhistFF;++k){ // To get also the systematics
-                h1_AI_FF[k]->Fill(FinalObservable,aweight*weight2*FF_weight);
-                //                }
+            if (is_boosted && mt<50 && q_1*q_2<0){
+                
+                if (antiisoRegion ){ //anti-isolated data, and MC without jets to subtract later
+                    //                for (int k=l;k<l+nbhistFF;++k){ // To get also the systematics
+                    h1_AI_FF[k]->Fill(FinalObservable,aweight*weight2*FF_weight);
+                    //                }
+                }
             }
-        }
-        //************************* Fill histograms Fake Factor VBF **********************
-        
-        if (is_VBF && mt<50 && q_1*q_2<0){
+            //************************* Fill histograms Fake Factor VBF **********************
             
-            if (antiisoRegion ){ //anti-isolated data, and MC without jets to subtract later
-                //                for (int k=l;k<l+nbhistFF;++k){ // To get also the systematics
-                h2_AI_FF[k]->Fill(FinalObservable,aweight*weight2*FF_weight);
-                //                }
+            if (is_VBF && mt<50 && q_1*q_2<0){
+                
+                if (antiisoRegion ){ //anti-isolated data, and MC without jets to subtract later
+                    //                for (int k=l;k<l+nbhistFF;++k){ // To get also the systematics
+                    h2_AI_FF[k]->Fill(FinalObservable,aweight*weight2*FF_weight);
+                    //                }
+                }
             }
         }
         
@@ -1880,7 +1896,7 @@ int main(int argc, char** argv) {
             h1_AI_FakeFraction_2D[k]->SetName(name.c_str()+postfixJES[k]+postfixFF[k]);
             h1_AI_FakeFraction_2D[k]->Write();
         }
-        esle{
+        else{
             //        for (int l=k; l<k+nbhistFF;++l){
             h1_AI_FF[k]->SetName(name.c_str()+postfixJES[k]+postfixFF[k]);
             h1_AI_FF[k]->Write();
